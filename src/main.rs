1595da4f63c490a02605d3cc3953ca1d4e0de0fe
Supreet <singh.supreet14@gmail.com>
2024-03-13 21:17:02 +0530

    Added the logging crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // This is the game main file. 
-use scrabble::client::{client_work, create_client, keypresses};
+use scrabble::client::{client_work, create_client};
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
+    env_logger::init(); 
 
     let mut c = create_client().await.expect("Failed to create client"); 
     client_work(&mut c).await;
