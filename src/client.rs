6ccc1db4ca4cefa353d85cdc0cffe904737fb58c
Supreet <singh.supreet14@gmail.com>
2024-03-14 21:00:54 +0530

    Fixed the server issues and also added raw_mode

--- src/client.rs
+++ src/client.rs
@@ -1,38 +1,40 @@
-use log::{debug, info};
-use tokio::{io::AsyncWriteExt, net::TcpStream};
-// use crossterm::event::{KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers}; 
-use crate::constants::global::{ClientEvent, KeyPress, KeyboardEvent, PORT};
+use std::process::exit;
 
-// create a client and bind to the given port. 
+use crate::constants::global::PORT;
+use crate::{Action, ClientEvent, KeyPress, KeyboardEvent};
+// use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use log::debug;
+use tokio::{io::AsyncWriteExt, net::TcpStream};
+
+// create a client and bind to the given port.
 pub async fn create_client() -> std::io::Result<TcpStream> {
-    TcpStream::connect(format!("0.0.0.0:{}",PORT)).await
+    TcpStream::connect(format!("0.0.0.0:{}", PORT)).await
 }
 
-
-// make then async, muliple keypresses are going here over time. 
-pub async fn client_work(client: &mut TcpStream) 
-{
+// make then async, muliple keypresses are going here over time.
+// Implement a channel where the client and server can communincate. 
+pub async fn client_work(client: &mut TcpStream) {
     loop {
-        // Use the keypresses to stream the data. 
-        if let Some(ce) = keypresses().await
-        {
+        // Use the keypresses to stream the data.
+        if let Some(ce) = keypresses().await {
+            if ce.action == Action::QUIT {
+                exit(0);
+            }
             let val = serde_json::to_string(&ce).expect("Failed to serialize");
             debug!("{}", val);
             let _ = client.write_all(val.trim().as_bytes()).await;
-            info!("Val sent");
         }
         let _ = client.flush().await;
     }
 }
 
-// the read function has to be made async. 
-// use the mpsc to check if the channel thing will work for keypresses. 
+// the read function has to be made async.
+// use the mpsc to check if the channel thing will work for keypresses.
 pub async fn keypresses() -> Option<ClientEvent> {
-    let mut keyboard_event = KeyboardEvent::new(); 
+    let mut keyboard_event = KeyboardEvent::new();
     let (rx, mut tx) = tokio::sync::mpsc::channel::<ClientEvent>(50);
     tokio::spawn(async move {
-        loop 
-        {
+        loop {
             if let KeyPress::Key(w) = keyboard_event.next().await.unwrap() {
                 rx.send(ClientEvent::from_key(&w)).await.expect("Failed");
             }
@@ -41,4 +43,3 @@ pub async fn keypresses() -> Option<ClientEvent> {
 
     tx.recv().await
 }
-
--- src/gameserver/bin/server.rs
+++ src/gameserver/bin/server.rs
@@ -1,30 +1,36 @@
-// This is the server main file. 
+// This is the server main file.
 use log::{debug, info};
-use tokio::{io::AsyncReadExt, net::{TcpListener, TcpStream}}; 
-use std::{char, net::SocketAddr};
-use scrabble::constants::global::{PORT, ClientEvent, Action}; 
+use scrabble::constants::global::PORT;
+use scrabble::ClientEvent;
+use std::net::SocketAddr;
+use tokio::{
+    io::AsyncReadExt,
+    net::{TcpListener, TcpStream},
+};
 
 type Connection = (TcpStream, SocketAddr);
 #[tokio::main]
-async fn main() 
-{
-    env_logger::init(); 
+
+
+async fn main() {
+    env_logger::init();
     println!("Running the server");
-    let listener = TcpListener::bind(format!("0.0.0.0:{}",PORT)).await.expect("Failed to bind to the port");
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", PORT))
+        .await
+        .expect("Failed to bind to the port");
 
     tokio::spawn(async move {
         match listener.accept().await {
-            Ok(mut connection) => 
-            {
+            Ok(mut connection) => {
                 debug!("Accepted connection {:?}", connection);
                 tokio::spawn(async move {
                     loop {
                         handle_connection(&mut connection).await;
                     }
                 });
-            }, 
+            }
             Err(e) => {
-                println!("Failed to accept the client because {}", e); 
+                println!("Failed to accept the client because {}", e);
             }
         }
     });
@@ -32,20 +38,17 @@ async fn main()
     loop {}
 }
 
-fn client_req(c_event: &ClientEvent) 
-{
-    println!("In the sever {:?}", c_event);
+fn client_req(c_event: &ClientEvent) {
+    info!("We are in the client req part {:?}", c_event);
 }
 
 // here the buffer is completely populating the 1024 bytes hence when it finds a null character it
-// crashes.  
-async fn handle_connection(con: &mut Connection) 
-{
+// crashes.
+async fn handle_connection(con: &mut Connection) {
     debug!("handle connection {:?}", con);
-    let (stream, _) = con; 
-    let mut buffer = [0u8; 1024]; 
-    let mut buf_cursor = 0; 
-
+    let (stream, _) = con;
+    let mut buffer = [0u8; 1024];
+    let mut buf_cursor = 0;
 
     match stream.read(&mut buffer).await {
         Ok(n) => {
@@ -65,6 +68,7 @@ async fn handle_connection(con: &mut Connection)
     }
 
     debug!("String {}", String::from_utf8_lossy(&buffer[0..buf_cursor]));
+
     if let Ok(c_event) = serde_json::from_slice::<ClientEvent>(&buffer[0..buf_cursor]) {
         client_req(&c_event);
     }
