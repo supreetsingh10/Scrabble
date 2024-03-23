137d408f033b1cce8499985c8112a69f36c28d67
Supreet <singh.supreet14@gmail.com>
2024-03-23 17:46:36 +0530

    Fixed the keypress issue, and refactored the code.

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,10 @@
+use core::panic;
 use std::process::exit;
 
-use crate::constants::global::PORT;
+use crate::constants::global::{DEBUG_LEVEL_1, PORT};
 use crate::{Action, ClientEvent, KeyPress, KeyboardEvent};
 // use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use log::debug;
+use log::{debug, info};
 use tokio::io::AsyncReadExt;
 use tokio::{io::AsyncWriteExt, net::TcpStream};
 use crate::Response;
@@ -16,23 +17,63 @@ pub async fn create_client() -> std::io::Result<TcpStream> {
 // make then async, muliple keypresses are going here over time.
 // Implement a channel where the client and server can communincate. 
 pub async fn client_update(client: &mut TcpStream) {
+    let mut keyboard_event = KeyboardEvent::new();
+    // let (rx, mut tx) = tokio::sync::mpsc::unbounded_channel::<ClientEvent>();
+
     loop {
         // Use the keypresses to stream the data.
-        if let Some(ce) = keypresses().await {
-            if ce.action == Action::QUIT {
-                exit(0);
+        let c_event = match keyboard_event.next().await {
+            Some(keypress) => {
+                match keypress {
+                    KeyPress::Key(k) => {
+                        ClientEvent::from_key(&k)
+                    },
+                    KeyPress::NONE => ClientEvent { action: Action::NONE },
+                    KeyPress::TICK => ClientEvent { action: Action::WAITING },
+                    KeyPress::ERROR => panic!("Error came check events"),
+                }
             }
+            None => {
+                    ClientEvent { action: Action::NONE }
+            }
+        };
 
-            let val = serde_json::to_string(&ce).expect("Failed to serialize");
-            let _ = client.write_all(val.trim().as_bytes()).await;
-            let _ = server_response(client).await;
-            // render;
-            // here the board will be updated and the changes will be rendered.
-        }
 
-        // this is where the client will process the server response and render the changes. 
+        // if player sack is empty then fill it up with values.
+        match c_event.action {
+            Action::NONE => {
+
+            },
+            Action::WAITING => {
+
+            },
+            Action::QUIT => {
+                let val = serde_json::to_string(&c_event).expect("Failed to serialize");
+                if let Err(e) = client.write_all(val.trim().as_bytes()).await {
+                    panic!("Failed to write the stream, server will not cut off Error: {}", e);
+                }
+                exit(0);
+            },
+            Action::WRITE(w) => {
+                let val = serde_json::to_string(&c_event).expect("Failed to serialize");
+
+                if let Err(e) = client.write_all(val.trim().as_bytes()).await {
+                    panic!("Failed to write the stream, server will not cut off Error: {}", e);
+                }
+
+                let resp = match server_response(client).await {
+                    Ok(r) => r,
+                    Err(e) => panic!("Error occured {}", e),
+                };
+            },
+            Action::DIRECTION(m) => {
+
+            }
+        };
+
         let _ = client.flush().await;
     }
+
 }
 
 async fn server_response(stream: &mut TcpStream) -> Result<Response, serde_json::Error> {
@@ -40,41 +81,17 @@ async fn server_response(stream: &mut TcpStream) -> Result<Response, serde_json:
     let mut buf_cursor = 0;
 
     debug!("We are on the client side and looking for response from the server");
-    match stream.read(&mut buffer).await {
-        Ok(n) => {
-            debug!("{} bytes read", n);
-        },
-        Err(_) => {
-            debug!("Failed to read");
-        }
-    }
+    let _ = stream.read(&mut buffer).await; 
 
     for (buf_sz, b_ch) in buffer.iter().enumerate() {
-        if b_ch.to_owned() == b'\0' {
+        if *b_ch == b'\0' {
             buf_cursor = buf_sz;
-            debug!("Broke the loop at {}", buf_cursor);
             break;
         }
     }
 
-    println!("{}", String::from_utf8_lossy(buffer[0..buf_cursor].as_ref()));
+    println!("Server respone. {}", String::from_utf8_lossy(buffer[0..buf_cursor].as_ref()));
 
     serde_json::from_slice(&buffer)
-
 }
 
-// the read function has to be made async.
-// use the mpsc to check if the channel thing will work for keypresses.
-pub async fn keypresses() -> Option<ClientEvent> {
-    let mut keyboard_event = KeyboardEvent::new();
-    let (rx, mut tx) = tokio::sync::mpsc::channel::<ClientEvent>(50);
-    tokio::spawn(async move {
-        loop {
-            if let KeyPress::Key(w) = keyboard_event.next().await.unwrap() {
-                rx.send(ClientEvent::from_key(&w)).await.expect("Failed");
-            }
-        }
-    });
-
-    tx.recv().await
-}
--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,6 @@
 pub mod global {
     pub const PORT: u32 = 8888;
+
+    pub const DEBUG_LEVEL_1: bool = false;
+    pub const DEBUG_LEVEL_2: bool = false;
 }
