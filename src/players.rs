d18a6c15a140e604316b356f27bb0e7b87732ff7
Supreet <singh.supreet14@gmail.com>
2024-03-23 17:45:26 +0530

    Moved players to client.

---
+++ src/players.rs
@@ -0,0 +1,25 @@
+use crate::PLAYER; 
+
+#[allow(dead_code)]
+pub struct Player {
+    player_num: PLAYER,
+    total_score: u32,
+    in_turn: bool,
+    letter_sack: [char; 7],
+}
+
+impl Player {
+    // Instantiates a new player. 
+    // The pool of letter sack will handled by the server and the current 
+    // player sack will be updated after every turn. 
+    // The number of characters in the sack will be 7. 
+    // I can use an array instead and I will keep updating the letters. 
+    pub fn new(p: PLAYER, l_s: [char; 7]) -> Self {
+        Player { 
+            player_num: p, 
+            total_score: 0,
+            in_turn: false,
+            letter_sack: l_s 
+        }
+    }
+}
